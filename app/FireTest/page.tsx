c8ebf14381cc252301148ebb3d65f84b3ea9afd0
admineral <50579369+admineral@users.noreply.github.com>
2024-08-01 18:15:48 +0200

    fixing firebase bug

--- app/FireTest/page.tsx
+++ app/FireTest/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import { app, db } from '../../firebase';
+import { app, db } from '@/lib/firebase';
 import { collection, getDocs } from 'firebase/firestore';
 
 interface Todo {
