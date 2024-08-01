73dd67f6672b013763fe543c0585e359aec8623c
admineral <50579369+admineral@users.noreply.github.com>
2024-08-01 17:57:04 +0200

    fixing firebase bug

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"C4SpdYtNORU1qyI7iqRGNpKNsgE87/qhTKqhStoclWY=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"HEU1P0GVEZmhOkEyiMDUhr2duJEOWhp56FYWFrlQtHw=\"}"
--- app/FireTest/page.tsx
+++ app/FireTest/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import { db } from "../../firebase";
+import { db } from "../firebase";
 import {
   collection,
   addDoc,
