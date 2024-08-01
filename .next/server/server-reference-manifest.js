9fa2dae8e4bf743c09b69c353e50993be2cd7836
admineral <50579369+admineral@users.noreply.github.com>
2024-08-01 17:13:26 +0200

    added FireBase and built basic To-Do app /FireTest page

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"UtSxo5klukwEmaVl4vkrrObIJKZuTAAA1v4UxK153/U=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"tRXPT5ccYJMJs2sWfsuSzufx4jk5Lber7qRCx22VnZM=\"\n}"
