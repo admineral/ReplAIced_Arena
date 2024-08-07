59a5023f892f380a8ab6af6b93d4a13ceff30927
admineral <50579369+admineral@users.noreply.github.com>
2024-08-07 13:52:35 +0200

    Gen3 Landingpage

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Sjhid1DfcpiAS37/Q1b1G80NddZGpC6QCzO/0eu/TEc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"JWZhKsNfF0JtGsQhPMth7PPa/wVXg6YbR2Qn6bibSH8=\"\n}"
