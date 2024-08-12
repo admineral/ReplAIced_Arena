58a2ffae7c7f73d2e8ec2105db33f5dc085cd3e8
admineral <50579369+admineral@users.noreply.github.com>
2024-08-12 17:56:53 +0200

    maxx sceen height arena

--- app/Arena/layout.tsx
+++ app/Arena/layout.tsx
@@ -3,12 +3,14 @@
 import React from 'react';
 import ControlPanel from '../../components/ControlPanel/ControlPanel_Component';
 import { useMapContext } from '../../contexts/MapContext';
+import { useMediaQuery } from 'react-responsive';
 
 export default function ArenaLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const isMobile = useMediaQuery({ maxWidth: 768 });
   const { 
     mode, 
     switchMode, 
@@ -27,25 +29,29 @@ export default function ArenaLayout({
   } = useMapContext();
 
   return (
-    <div className="arena-layout">
-      <ControlPanel 
-        mode={mode}
-        switchMode={switchMode}
-        openCreateBoxModal={openCreateBoxModal}
-        reloadBoxes={reloadBoxes}
-        clearAllBoxes={clearAllBoxes}
-        isAttackModeAvailable={isAttackModeAvailable}
-        isLoading={isLoading}
-        setLastUpdateTime={setLastUpdateTime}
-        onBoxCreated={reloadBoxes}
-        mapPosition={mapPosition}
-        mapZoom={mapZoom}
-        onMapPositionChange={handleMapPositionChange}
-        onMapZoomChange={handleMapZoomChange}
-        boxCount={boxes.length}
-        lastUpdateTime={lastUpdateTime}
-      />
-      {children}
+    <div className="arena-layout flex flex-col h-screen overflow-hidden">
+      <div className="flex-shrink-0">
+        <ControlPanel 
+          mode={mode}
+          switchMode={switchMode}
+          openCreateBoxModal={openCreateBoxModal}
+          reloadBoxes={reloadBoxes}
+          clearAllBoxes={clearAllBoxes}
+          isAttackModeAvailable={isAttackModeAvailable}
+          isLoading={isLoading}
+          setLastUpdateTime={setLastUpdateTime}
+          onBoxCreated={reloadBoxes}
+          mapPosition={mapPosition}
+          mapZoom={mapZoom}
+          onMapPositionChange={handleMapPositionChange}
+          onMapZoomChange={handleMapZoomChange}
+          boxCount={boxes.length}
+          lastUpdateTime={lastUpdateTime}
+        />
+      </div>
+      <div className={`flex-grow ${isMobile ? 'h-[calc(100vh-64px)]' : ''} overflow-hidden`}>
+        {children}
+      </div>
     </div>
   );
 }
